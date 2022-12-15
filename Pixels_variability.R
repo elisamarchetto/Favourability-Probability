3114bdb584ccb670b038fcd45b5848311822b851
Elisa Marchetto <62661809+elisamarchetto@users.noreply.github.com>
2022-12-15 16:57:50 +0100

    Update Pixels_variability.R

--- Pixels_variability.R
+++ Pixels_variability.R
@@ -233,7 +233,7 @@ p <- ggplot(df.Tot, aes(model, values, fill = sampling))+
   geom_half_boxplot(nudge = 0.05, outlier.color = NA, side = "r")+
   geom_hline(aes(yintercept = 0), color = "gray70", size = 0.6) +
   scale_fill_manual( values=c("#D55E00", "#0072B2"))+
-  labs(x="Model", y="CV difference value", fill="Sampling method")+
+  labs(x="Model", y="Difference value between CVs", fill="Sampling method")+
   scale_x_discrete(labels=pp, limits=c("GLM A","GLM B", "GAM A", "GAM B", "RF A", "RF B","BRT A","BRT B"))+
   theme_light()+
   theme(legend.background=element_blank(),
@@ -286,163 +286,266 @@ ggsave(plot = p,
      names(stack_diffS) <- c("GLM", "GAM", "RF","BRT")
      
      
-     s1<-gplot(stack_probS) + 
-       geom_tile(aes(fill = value)) +
-       facet_wrap(~ variable, nrow = 4) + 
-       scale_fill_scico(palette = "batlow",direction = 1,alpha = 0.7, limits=c(0.00000000,223.6068),
-                        oob = scales::squish, na.value="transparent")+
-       labs(x="Longitude",y="Latitude", fill="CV", title="Probability")+
-       theme_light()+
-       theme(plot.margin = margin(.3,.3,.3,.3, "cm"),
-             legend.position = "bottom",  
-             plot.title = element_text(size=15,face = 'bold',hjust = 0.5),
-             legend.title=element_text(size=12,face = 'bold'),
-             panel.grid.major = element_blank(),
-             panel.grid.minor = element_blank(),
-             panel.background = element_blank(),
-             axis.title.x = element_text(size=10,face = 'bold'),
-             axis.text.x = element_text(size = 8.5, face = 'bold'),
-             axis.title.y = element_text(size=10,face = 'bold'),
-             axis.text.y = element_text(size = 8.5, face = 'bold'),
-             axis.ticks.y=element_blank())+
-       guides(fill = guide_colourbar(title.position="top", title.hjust = 0.5, barwidth = 10, barheight = 0.6),
-              size = guide_legend(title.position="top", title.hjust = 0.5))+
-       coord_equal(xlim = c(-26, 41), ylim = c(30, 72), expand = TRUE)
-     
-     s2<-gplot(stack_favS) + 
-       geom_tile(aes(fill = value)) +
-       facet_wrap(~ variable, nrow = 4) + 
-       scale_fill_scico(palette = "batlow",direction = 1,alpha = 0.7, limits=c(0.00000000,223.6068),
-                        oob = scales::squish, na.value="transparent")+
-       labs(x="Longitude",y="Latitude", fill="CV", title="Favourability")+
-       theme_light()+
-       theme(plot.margin = margin(.3,.3,.3,.3, "cm"),
-             legend.position = "bottom",  
-             plot.title = element_text(size=15,face = 'bold',hjust = 0.5),
-             legend.title=element_text(size=12,face = 'bold'),
-             panel.grid.major = element_blank(),
-             panel.grid.minor = element_blank(),
-             panel.background = element_blank(),
-             axis.title.x = element_text(size=10,face = 'bold'),
-             axis.text.x = element_text(size = 8.5, face = 'bold'),
-             axis.title.y = element_text(size=10,face = 'bold'),
-             axis.text.y = element_text(size = 8.5, face = 'bold'),
-             axis.ticks.y=element_blank())+
-       guides(fill = guide_colourbar(title.position="top", title.hjust = 0.5, barwidth = 10, barheight = 0.6),
-              size = guide_legend(title.position="top", title.hjust = 0.5))+
-       coord_equal(xlim = c(-26, 41), ylim = c(30, 72), expand = TRUE) 
-      
-     s3<-gplot(stack_diffS) + 
-       geom_tile(aes(fill = value)) +
-       facet_wrap(~ variable, nrow = 4) + 
-       scale_fill_scico(palette = "batlow",direction = 1,alpha = 0.7, limits=c(-64.161011,49.23872),
-                        oob = scales::squish, na.value="transparent")+
-       labs(x="Longitude",y="Latitude", fill="Difference values", title="Difference")+
-       theme_light()+
-       theme(plot.margin = margin(.3,.3,.3,.3, "cm"),
-         legend.position = "bottom",  
-             plot.title = element_text(size=15,face = 'bold',hjust = 0.5),
-             legend.title=element_text(size=12,face = 'bold'),
-  
-             panel.grid.major = element_blank(),
-             panel.grid.minor = element_blank(),
-             panel.background = element_blank(),
-             axis.title.x = element_text(size=10,face = 'bold'),
-             axis.text.x = element_text(size = 8.5, face = 'bold'),
-             axis.title.y = element_text(size=10,face = 'bold'),
-             axis.text.y = element_text(size = 8.5, face = 'bold'),
-             axis.ticks.y=element_blank())+
-       guides(fill = guide_colourbar(title.position="top", title.hjust = 0.5, barwidth = 10, barheight = 0.6),
-              size = guide_legend(title.position="top", title.hjust = 0.5))+
-       coord_equal(xlim = c(-26, 41), ylim = c(30, 72), expand = TRUE)
-  
-    s <- s1 + s2 + s3 + plot_annotation(title = "Stratified sampling")
-    
-    
-    
-    
-    
-    r1<-gplot(stack_probR) + 
-      geom_tile(aes(fill = value)) +
-      facet_wrap(~ variable, nrow = 4) + 
-      scale_fill_scico(palette = "batlow",direction = 1,alpha = 0.7, limits=c(0.00000000,223.6068),
-                       oob = scales::squish, na.value="transparent")+
-      labs(x="Longitude",y="Latitude", fill="CV", title="Probability")+
-      theme_light()+
-      theme(plot.margin = margin(.3,.3,.3,.3, "cm"),
-            legend.position = "bottom",  
-            plot.title = element_text(size=15,face = 'bold',hjust = 0.5),
-            legend.title=element_text(size=12,face = 'bold'),
-            panel.grid.major = element_blank(),
-            panel.grid.minor = element_blank(),
-            panel.background = element_blank(),
-            axis.title.x = element_text(size=10,face = 'bold'),
-            axis.text.x = element_text(size = 8.5, face = 'bold'),
-            axis.title.y = element_text(size=10,face = 'bold'),
-            axis.text.y = element_text(size = 8.5, face = 'bold'),
-            axis.ticks.y=element_blank())+
-      guides(fill = guide_colourbar(title.position="top", title.hjust = 0.5, barwidth = 10, barheight = 0.6),
-             size = guide_legend(title.position="top", title.hjust = 0.5))+
-      coord_equal(xlim = c(-26, 41), ylim = c(30, 72), expand = TRUE)
-    
-    r2<-gplot(stack_favR) + 
-      geom_tile(aes(fill = value)) +
-      facet_wrap(~ variable, nrow = 4) + 
-      scale_fill_scico(palette = "batlow",direction = 1,alpha = 0.7, limits=c(0.00000000,223.6068),
-                       oob = scales::squish, na.value="transparent")+
-      labs(x="Longitude",y="Latitude", fill="CV", title="Favourability")+
-      theme_light()+
-      theme(plot.margin = margin(.3,.3,.3,.3, "cm"),
-            legend.position = "bottom",  
-            plot.title = element_text(size=15,face = 'bold',hjust = 0.5),
-            legend.title=element_text(size=12,face = 'bold'),
-            panel.grid.major = element_blank(),
-            panel.grid.minor = element_blank(),
-            panel.background = element_blank(),
-            axis.title.x = element_text(size=10,face = 'bold'),
-            axis.text.x = element_text(size = 8.5, face = 'bold'),
-            axis.title.y = element_text(size=10,face = 'bold'),
-            axis.text.y = element_text(size = 8.5, face = 'bold'),
-            axis.ticks.y=element_blank())+
-      guides(fill = guide_colourbar(title.position="top", title.hjust = 0.5, barwidth = 10, barheight = 0.6),
-             size = guide_legend(title.position="top", title.hjust = 0.5))+
-      coord_equal(xlim = c(-26, 41), ylim = c(30, 72), expand = TRUE) 
-    
-    r3<-gplot(stack_diffR) + 
-      geom_tile(aes(fill = value)) +
-      facet_wrap(~ variable, nrow = 4) + 
-      scale_fill_scico(palette = "batlow",direction = 1,alpha = 0.7, limits=c(-66.447537, 50.29264),
-                       oob = scales::squish, na.value="transparent")+
-      labs(x="Longitude",y="Latitude", fill="Difference values", title="Difference")+
-      theme_light()+
-      theme(plot.margin = margin(.3,.3,.3,.3, "cm"),
-            legend.position = "bottom",  
-            plot.title = element_text(size=15,face = 'bold',hjust = 0.5),
-            legend.title=element_text(size=12,face = 'bold'),
-            
-            panel.grid.major = element_blank(),
-            panel.grid.minor = element_blank(),
-            panel.background = element_blank(),
-            axis.title.x = element_text(size=10,face = 'bold'),
-            axis.text.x = element_text(size = 8.5, face = 'bold'),
-            axis.title.y = element_text(size=10,face = 'bold'),
-            axis.text.y = element_text(size = 8.5, face = 'bold'),
-            axis.ticks.y=element_blank())+
-      guides(fill = guide_colourbar(title.position="top", title.hjust = 0.5, barwidth = 10, barheight = 0.6),
-             size = guide_legend(title.position="top", title.hjust = 0.5))+
-      coord_equal(xlim = c(-26, 41), ylim = c(30, 72), expand = TRUE)
-    
-    r <- r1 + r2 + r3 + plot_annotation(title = "Random sampling")
-    
-    ggsave(plot = r,
-           filename = "r.png",
-           width = 10,
-           height = 10,
-           dpi = 300)
-    ggsave(plot = s,
-           filename = "s.png",
-           width = 10,
-           height = 10,
-           dpi = 300) 
-    
+      stack_probS_df <-
+  as.data.frame(stack_probS, xy = TRUE) %>%
+  na.omit()
 
+GGstack_probS_df <- 
+  stack_probS_df %>%
+  pivot_longer(
+    c(-x, -y),
+    names_to = "variable",
+    values_to = "value")
+
+stack_probR_df <-
+  as.data.frame(stack_probR, xy = TRUE) %>%
+  na.omit()
+
+GGstack_probR_df <- 
+  stack_probR_df %>%
+  pivot_longer(
+    c(-x, -y),
+    names_to = "variable",
+    values_to = "value")
+
+stack_favS_df <-
+  as.data.frame(stack_favS, xy = TRUE) %>%
+  na.omit()
+
+GGstack_favS_df <- 
+  stack_favS_df %>%
+  pivot_longer(
+    c(-x, -y),
+    names_to = "variable",
+    values_to = "value")
+
+stack_favR_df <-
+  as.data.frame(stack_favR, xy = TRUE) %>%
+  na.omit()
+
+GGstack_favR_df <- 
+  stack_favR_df %>%
+  pivot_longer(
+    c(-x, -y),
+    names_to = "variable",
+    values_to = "value")
+
+
+
+stack_diffS_df <-
+  as.data.frame(stack_diffS, xy = TRUE) %>%
+  na.omit()
+
+GGstack_diffS_df <- 
+  stack_diffS_df %>%
+  pivot_longer(
+    c(-x, -y),
+    names_to = "variable",
+    values_to = "value")
+
+
+stack_diffR_df <-
+  as.data.frame(stack_diffR, xy = TRUE) %>%
+  na.omit()
+
+GGstack_diffR_df <- 
+  stack_diffR_df %>%
+  pivot_longer(
+    c(-x, -y),
+    names_to = "variable",
+    values_to = "value")
+
+
+######################
+######################
+
+
+s1<- GGstack_probS_df %>%
+  mutate(across(variable, factor, levels=c("GLM","GAM","RF","BRT"))) %>%
+  ggplot() +
+  geom_tile(aes(x = x, y = y, fill = value)) +
+  facet_wrap(~ variable, nrow = 4) + 
+  geom_sf(data=world,
+          colour = "black", fill = "transparent", size=0.3)+  
+  scale_fill_scico(palette = "batlow",direction = 1,alpha = 0.7, limits=c(0.00000000,223.6068),
+                   oob = scales::squish, na.value="transparent")+
+  labs(x="Longitude",y="Latitude", fill="CV", title="Probability")+
+  theme_light()+
+  theme(
+    legend.position = "bottom",  
+    plot.title = element_text(size=30,face = 'bold',hjust = 0.5),
+    legend.title=element_text(size=20,face = 'bold'),
+    legend.text = element_text(size=18,face = 'bold'),
+    panel.grid.major = element_blank(),
+    panel.grid.minor = element_blank(),
+    panel.background = element_blank(),
+    axis.title.x = element_text(size=19,face = 'bold'),
+    axis.text.x = element_text(size = 19, face = 'bold'),
+    axis.title.y = element_text(size=23,face = 'bold'),
+    axis.text.y = element_text(size = 19, face = 'bold'),
+    axis.ticks.y=element_blank(),
+    strip.text = element_text(size = 18,face = 'bold', colour = "black"))+
+  guides(fill = guide_colourbar(title.position="top", title.hjust = 0.5, barwidth = 21, barheight = 1.9),
+         size = guide_legend(title.position="top", title.hjust = 0.5))+
+coord_sf(xlim = c(-26, 41), ylim = c(32, 72), expand = TRUE)
+
+
+s2<- GGstack_favS_df %>%
+  mutate(across(variable, factor, levels=c("GLM","GAM","RF","BRT"))) %>%
+  ggplot() +
+  geom_tile(aes(x = x, y = y, fill = value)) +
+  facet_wrap(~ variable, nrow = 4) + 
+  geom_sf(data=world,
+          colour = "black", fill = "transparent", size=0.3)+  
+  scale_fill_scico(palette = "batlow",direction = 1,alpha = 0.7, limits=c(0.00000000,223.6068),
+                   oob = scales::squish, na.value="transparent")+
+  labs(x="Longitude",y="Latitude", fill="CV", title="Favourability")+
+  theme_light()+
+  theme(
+    legend.position = "bottom",  
+    plot.title = element_text(size=30,face = 'bold',hjust = 0.5),
+    legend.title=element_text(size=20,face = 'bold'),
+    legend.text = element_text(size=18,face = 'bold'),
+    panel.grid.major = element_blank(),
+    panel.grid.minor = element_blank(),
+    panel.background = element_blank(),
+    axis.title.x = element_text(size=19,face = 'bold'),
+    axis.text.x = element_text(size = 19, face = 'bold'),
+    axis.title.y = element_text(size=23,face = 'bold'),
+    axis.text.y = element_text(size = 19, face = 'bold'),
+    axis.ticks.y=element_blank(),
+    strip.text = element_text(size = 18,face = 'bold', colour = "black"))+
+  guides(fill = guide_colourbar(title.position="top", title.hjust = 0.5, barwidth = 21, barheight = 1.9),
+         size = guide_legend(title.position="top", title.hjust = 0.5))+
+  coord_sf(xlim = c(-26, 41), ylim = c(32, 72), expand = TRUE)
+
+s3<- GGstack_diffS_df %>%
+  mutate(across(variable, factor, levels=c("GLM","GAM","RF","BRT"))) %>%
+  ggplot() +
+  geom_tile(aes(x = x, y = y, fill = value)) +
+  facet_wrap(~ variable, nrow = 4) + 
+  geom_sf(data=world,
+          colour = "black", fill = "transparent", size=0.3)+  
+  scale_fill_scico(palette = "batlow",direction = 1,alpha = 0.7, limits=c(-64.161011,49.23872),
+                   oob = scales::squish, na.value="transparent")+
+  labs(x="Longitude",y="Latitude", fill="CV", title="Difference")+
+  theme_light()+
+  theme(
+    legend.position = "bottom",  
+    plot.title = element_text(size=30,face = 'bold',hjust = 0.5),
+    legend.title=element_text(size=20,face = 'bold'),
+    legend.text = element_text(size=18,face = 'bold'),
+    panel.grid.major = element_blank(),
+    panel.grid.minor = element_blank(),
+    panel.background = element_blank(),
+    axis.title.x = element_text(size=19,face = 'bold'),
+    axis.text.x = element_text(size = 19, face = 'bold'),
+    axis.title.y = element_text(size=23,face = 'bold'),
+    axis.text.y = element_text(size = 19, face = 'bold'),
+    axis.ticks.y=element_blank(),
+    strip.text = element_text(size = 18,face = 'bold', colour = "black"))+
+  guides(fill = guide_colourbar(title.position="top", title.hjust = 0.5, barwidth = 21, barheight = 1.9),
+         size = guide_legend(title.position="top", title.hjust = 0.5))+
+  coord_sf(xlim = c(-26, 41), ylim = c(32, 72), expand = TRUE)
+
+s <- s1 + s2 + s3  + plot_annotation(title = "Stratified sampling", theme = theme(plot.title = element_text(size = 25, face = 'bold')))
+       
+ggsave(plot = s,
+       filename = "s2.jpg",
+       width = 25,
+       height = 25,
+       dpi = 600)
+
+r1<- GGstack_probR_df %>%
+  mutate(across(variable, factor, levels=c("GLM","GAM","RF","BRT"))) %>%
+  ggplot() +
+  geom_tile(aes(x = x, y = y, fill = value)) +
+  facet_wrap(~ variable, nrow = 4) + 
+  geom_sf(data=world,
+          colour = "black", fill = "transparent", size=0.3)+  
+  scale_fill_scico(palette = "batlow",direction = 1,alpha = 0.7, limits=c(0.00000000,223.6068),
+                   oob = scales::squish, na.value="transparent")+
+  labs(x="Longitude",y="Latitude", fill="CV", title="Probability")+
+  theme_light()+
+  theme(
+    legend.position = "bottom",  
+    plot.title = element_text(size=30,face = 'bold',hjust = 0.5),
+    legend.title=element_text(size=20,face = 'bold'),
+    legend.text = element_text(size=18,face = 'bold'),
+    panel.grid.major = element_blank(),
+    panel.grid.minor = element_blank(),
+    panel.background = element_blank(),
+    axis.title.x = element_text(size=19,face = 'bold'),
+    axis.text.x = element_text(size = 19, face = 'bold'),
+    axis.title.y = element_text(size=23,face = 'bold'),
+    axis.text.y = element_text(size = 19, face = 'bold'),
+    axis.ticks.y=element_blank(),
+    strip.text = element_text(size = 18,face = 'bold', colour = "black"))+
+  guides(fill = guide_colourbar(title.position="top", title.hjust = 0.5, barwidth = 21, barheight = 1.9),
+  coord_sf(xlim = c(-26, 41), ylim = c(32, 72), expand = TRUE)
+
+
+r2<- GGstack_favR_df %>%
+  mutate(across(variable, factor, levels=c("GLM","GAM","RF","BRT"))) %>%
+  ggplot() +
+  geom_tile(aes(x = x, y = y, fill = value)) +
+  facet_wrap(~ variable, nrow = 4) + 
+  geom_sf(data=world,
+          colour = "black", fill = "transparent", size=0.3)+  
+  scale_fill_scico(palette = "batlow",direction = 1,alpha = 0.7, limits=c(0.00000000,223.6068),
+                   oob = scales::squish, na.value="transparent")+
+  labs(x="Longitude",y="Latitude", fill="CV", title="Favourability")+
+  theme_light()+
+  theme(
+    legend.position = "bottom",  
+    plot.title = element_text(size=30,face = 'bold',hjust = 0.5),
+    legend.title=element_text(size=20,face = 'bold'),
+    legend.text = element_text(size=18,face = 'bold'),
+    panel.grid.major = element_blank(),
+    panel.grid.minor = element_blank(),
+    panel.background = element_blank(),
+    axis.title.x = element_text(size=19,face = 'bold'),
+    axis.text.x = element_text(size = 19, face = 'bold'),
+    axis.title.y = element_text(size=23,face = 'bold'),
+    axis.text.y = element_text(size = 19, face = 'bold'),
+    axis.ticks.y=element_blank(),
+    strip.text = element_text(size = 18,face = 'bold', colour = "black"))+
+  guides(fill = guide_colourbar(title.position="top", title.hjust = 0.5, barwidth = 21, barheight = 1.9),
+  coord_sf(xlim = c(-26, 41), ylim = c(32, 72), expand = TRUE)
+
+r3<- GGstack_diffR_df %>%
+  mutate(across(variable, factor, levels=c("GLM","GAM","RF","BRT"))) %>%
+  ggplot() +
+  geom_tile(aes(x = x, y = y, fill = value)) +
+  facet_wrap(~ variable, nrow = 4) + 
+  geom_sf(data=world,
+          colour = "black", fill = "transparent", size=0.3)+  
+  scale_fill_scico(palette = "batlow",direction = 1,alpha = 0.7, limits=c(-65.900472,47.70545),
+                   oob = scales::squish, na.value="transparent")+
+  labs(x="Longitude",y="Latitude", fill="CV", title="Difference")+
+  theme_light()+
+  theme(
+    legend.position = "bottom",  
+    plot.title = element_text(size=30,face = 'bold',hjust = 0.5),
+    legend.title=element_text(size=20,face = 'bold'),
+    legend.text = element_text(size=18,face = 'bold'),
+    panel.grid.major = element_blank(),
+    panel.grid.minor = element_blank(),
+    panel.background = element_blank(),
+    axis.title.x = element_text(size=19,face = 'bold'),
+    axis.text.x = element_text(size = 19, face = 'bold'),
+    axis.title.y = element_text(size=23,face = 'bold'),
+    axis.text.y = element_text(size = 19, face = 'bold'),
+    axis.ticks.y=element_blank(),
+    strip.text = element_text(size = 18,face = 'bold', colour = "black"))+
+  guides(fill = guide_colourbar(title.position="top", title.hjust = 0.5, barwidth = 21, barheight = 1.9),
+         size = guide_legend(title.position="top", title.hjust = 0.5))+
+  coord_sf(xlim = c(-26, 41), ylim = c(32, 72), expand = TRUE)
+
+r <- r1 + r2 + r3  + plot_annotation(title = "Random sampling", theme = theme(plot.title = element_text(size = 25, face = 'bold')))
+
+ggsave(plot = r,
+       filename = "r2.jpg",
+       width = 25,
+       height = 25,
+       dpi = 600)
